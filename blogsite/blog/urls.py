4cbfb273d5b6436a32c4d0e97b552e0d9a52300b
pystart-school <pystart@yahoo.com>
2025-03-18 15:24:37 +0400

    Added functionality to edit, delete post and styled pages.

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('create/', views.post_create, name='post_create'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/edit/<int:pk>/', views.post_edit, name='post_edit'),
+    path('post/delete/<int:pk>/', views.post_delete, name='post_delete'),
 ]
--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -22,3 +22,27 @@ def post_create(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_edit(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_delete.html', {'post': post})
+
+
+
